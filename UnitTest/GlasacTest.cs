13fd919d74d3c5f9844948e3c627efcfa29c5e3c
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2022-12-07 16:22:34 +0100

    Kreirana testni projekat i klasa

---
+++ UnitTest/GlasacTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineGlasanje;
+using System;
+
+/*
+ * Elvir Vlahovljak
+ * 18702
+ */
+
+namespace UnitTest
+{
+    [TestClass]
+    public class GlasacTest
+    {
+        static Glasač glasac;
+
+        [TestInitialize] 
+        public void InicijalizacijaPrijeSvakogTesta()
+        {
+            glasac = new Glasač("Elvir", "Vlahovljak", "Tamo negdje 1", DateTime.Parse("24/01/2000"), "EJK1MTE", "2401000150004");
+        }
+
+    }
+}
