1837e7b7acc8ba3b74f9540842ab603f11414b36
mkokor2 <mkokor2@etf.unsa.ba>
2022-12-08 16:17:20 +0100

    Kreirana projekat za unit testiranje

---
+++ UnitTestovi/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestovi
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
