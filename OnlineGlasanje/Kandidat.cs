24616ffe52de9d6c6f8fa3630d20af8d890388c9
mkokor2 <mkokor2@etf.unsa.ba>
2022-12-08 15:11:18 +0100

    Dodane metode za manipulaciju sa članstvom kandidata u stranci, tj. metode DaLiJeČlanStranke, ZavršiTrenutnoČlanstvo i Učlani se (dodane u klasu Kandidat)

--- OnlineGlasanje/Kandidat.cs
+++ OnlineGlasanje/Kandidat.cs
@@ -1,4 +1,7 @@
-﻿namespace OnlineGlasanje
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineGlasanje
 {
     public class Kandidat
     {
@@ -6,33 +9,64 @@
         #region Atributi
         
         string ime, prezime;
-        Stranka stranka;
+        Stranka trenutnaStranka;
         int brojGlasova;
+        List<EvidencijaČlanstva> evidencijeČlanstava;
 
         #endregion
 
 
         #region Properties
+
         public string Ime { get => ime; set => ime = value; }
         public string Prezime { get => prezime; set => prezime = value; }
-        public Stranka Stranka { get => stranka; set => stranka = value; } 
+        public Stranka TrenutnaStranka { get => trenutnaStranka; set => trenutnaStranka = value; } 
         public int BrojGlasova { get => brojGlasova; set => brojGlasova = value; }
+        public List<EvidencijaČlanstva> EvidencijeČlanstava { get => evidencijeČlanstava; set => evidencijeČlanstava = value; }
 
         #endregion
 
 
         #region Konstruktor
 
-        public Kandidat(string ime, string prezime, Stranka stranka = null) 
+        public Kandidat(string ime, string prezime, Stranka trenutnaStranka = null) 
         { 
             this.ime = ime;
             this.prezime = prezime;
-            this.stranka = stranka;
+            EvidencijeČlanstava = new List<EvidencijaČlanstva>();
+            if (trenutnaStranka != null)
+                UčlaniSe(trenutnaStranka);
+            else
+                TrenutnaStranka = null;
             brojGlasova = 0;
         }
 
         #endregion
 
+
+        #region Metode
+
+        // Ova metoda provjerava da li je kandidat član bilo koje stranke. 
+        public bool DaLiJeČlanStranke()
+        {
+            return TrenutnaStranka != null;
+        }
+
+        public void ZavršiTrenutnoČlanstvo()
+        {
+            EvidencijeČlanstava.FindLast(evidencija => evidencija.Stranka.Naziv.Equals(TrenutnaStranka.Naziv))
+                               .DatumZavršetkaČlanstva = DateTime.Now;
+        }
+
+        public void UčlaniSe(Stranka stranka)
+        {
+            if (DaLiJeČlanStranke())
+                throw new InvalidOperationException("Kandidat je već član stranke " + TrenutnaStranka.Naziv + "!");
+            TrenutnaStranka = stranka;
+            EvidencijeČlanstava.Add(new EvidencijaČlanstva(stranka, DateTime.Now));
+        }
+
+        #endregion
 
     }
 }
